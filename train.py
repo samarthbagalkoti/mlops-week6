bbfe91b200ce087dfb49bb8896267a48b26efae8
samarthbagalkoti <samarth.bagalkoti@gmail.com>
2025-08-28 11:42:18 +0000

    W6-D2

--- train.py
+++ train.py
@@ -9,26 +9,27 @@ mlflow.set_tracking_uri("http://54.91.173.39:8081/")
 mlflow.set_experiment("week6")
 
 # Load dataset
-data = pd.read_csv("data.csv")
-X = data[["feature"]]
-y = data["target"]
-
-# Train simple model
-model = LinearRegression()
-model.fit(X, y)
-
-# Save model
-joblib.dump(model, "model.pkl")
-
-# Log with MLflow
 with mlflow.start_run():
-    # metrics
-    r2 = model.score(X, y)
-    mlflow.log_metric("R2", r2)
+    # Load dataset
+    data = pd.read_csv("data.csv")
+    X = data[["feature"]]
+    y = data["target"]
 
-    # log artifact
-    mlflow.log_artifact("model.pkl")
+    # Train simple model
+    model = LinearRegression()
+    model.fit(X, y)
 
-    # optionally log params
-    mlflow.log_param("fit_intercept", model.fit_intercept)
+    # Save model locally
+    joblib.dump(model, "model.pkl")
 
+    # Custom metric logging (in addition to autolog)
+    r2_score = model.score(X, y)
+    mlflow.log_metric("r2_score", r2_score)
+
+    # Log params
+    mlflow.log_param("model_type", "LinearRegression")
+
+    # Log artifact (metrics file)
+    with open("metrics.txt", "w") as f:
+        f.write(f"R2: {r2_score}\n")
+    mlflow.log_artifact("metrics.txt")
